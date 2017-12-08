fd907c67158991306aec66ad8517313c7cc6a1c0
(s) Jasmine Price <jprice3@pm7-0078.uopnet.plymouth.ac.uk>
2017-12-08 16:17:10 +0000

    Seventh Commit

--- sketch.js
+++ sketch.js
@@ -1,65 +1,79 @@
 //JasminePrice - 405 - Creative Coding Project
 
 //Setup of variables
-let x;
-let y;
-let ellipseSize;
-let angleX = 0;
-let sinValue;
-let mapSin;
-let sinValue2;
-let mapSin2;
-let r, g, b = (255); //Initialize r,g,b each to 255
+let circleObject1;
+let circleObject2;
+let circleObject3;
 
 function setup(){
   var canvas = createCanvas(594, 841); //Use this function to define the size of the output window
   canvas.parent("myContainer");
-  x = random(0, 594);
-  y = 0;
-  ellipseSize = (random(10, 50));
-  r = random (255);
-  g = random (255);
-  b = random (255);
-  noStroke();
+
+  circleObject1 = new Circle(random(0, 594), 0, 0.02, random(0.01, 0.08), random(10, 50));
+  circleObject2 = new Circle(random(0, 594), 0, 0.02, random(0.01, 0.08),  random(10, 50));
+  circleObject3 = new Circle(random(0, 594), 0, 0.02, random(0.01, 0.08),  random(10, 50));
 }
 
 function draw(){
-  fill(160, 10);
-  //rect(0, 0, width, height);
+  fill(0, 10);
+  rect(0, 0, width, height);
   //background(0);
-  move();
-  show();
-
+  circleObject1.moveFunction();
+  circleObject1.displayCircle();
+  circleObject2.moveFunction();
+  circleObject2.displayCircle();
+  circleObject3.moveFunction();
+  circleObject3.displayCircle();
 }
+
+class Circle{
+
+    constructor(x, y, angleX, freq, size){
+      this.x = x;
+      this.y = y;
+      this.angleX = angleX;
+      this.freq = freq;
+      this.size = size;
+
+      this.rd = random(255);
+      this.grn = random(255);
+      this.bl = random(255);
+      this.a = 255;
+    }
+
+
 // The function that controls the movement of the ball
-function move(){
-  y = y + 1; //Add +1 pixel to the position of x (for every frame)
+moveFunction(){
+  this.y = this.y + 1; //Add +1 pixel to the position of x (for every frame)
   //console.log(x);
   //If the position of the ellipse reaches the limit of the canvas,
   //then reset its position to 0 - ellipse appears from the top again
-  angleX+=0.02; // speed of the wave
-  sinValue = sin(angleX);
-  mapSin = map(sinValue, -1, 1, -50, 50); // amplitude
+  this.angleX+=this.freq; // speed of the wave
+  this.sinValue = sin(this.angleX);
+  this.mapSin = map(this.sinValue, -1, 1, -50, 50); // amplitude
 
-  sinValue2 = sin(angleX);
-  mapSin2 = map(sinValue2, -1, 1, 50, -50);
+  //this.sinValue2 = sin(this.angleX);
+  //this.mapSin2 = map(this.sinValue2, -1, 1, 50, -50);
 
-  if (y > 866){ //866 comes from 841 (height) + 25 (half size of ellipse)
-    r = random (255);
-    g = random (255);
-    b = random (255);
-    ellipseSize = (random(10, 50));
-    x = random(0, 594);
-    y = -25; //instead reset to 0, we reset to 0-25 (half size of ellipse)
+  if (this.y > 866){ //866 comes from 841 (height) + 25 (half size of ellipse)
+    this.rd = random (255);
+    this.grn = random (255);
+    this.bl = random (255);
+    this.a = 255;
+    this.size = (random(10, 50));
+    this.x = random(0, 594);
+    this.y = -25; //instead reset to 0, we reset to 0-25 (half size of ellipse)
   }
 
-  console.log(x+mapSin);
+  //console.log(x+mapSin);
   //We only change the position of x (y is static to middle)
 }
 // The function that displays and controls the ellipse parameters
-function show(){
-  fill(r, g, b, 80);
-  ellipse(x+mapSin, y, ellipseSize, ellipseSize);
-
-  ellipse(x+mapSin2, y, ellipseSize, ellipseSize);
+displayCircle(){
+  noStroke();
+  this.fillcol = color(this.rd, this.grn, this.bl, this.a)
+  fill(this.fillcol);
+  ellipse(this.x+this.mapSin, this.y, this.size, this.size);
+  //ellipse(this.x+this.mapSin2, this.y, this.size, this.size);
+}
 }
