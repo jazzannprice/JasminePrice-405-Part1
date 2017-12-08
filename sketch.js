824323c5a261a409247a239b9b47dc3f78bd19b1
(s) Jasmine Price <jprice3@pm7-0078.uopnet.plymouth.ac.uk>
2017-12-08 16:20:08 +0000

    Eighth Commit

--- sketch.js
+++ sketch.js
@@ -1,39 +1,35 @@
 //JasminePrice - 405 - Creative Coding Project
 
 //Setup of variables
-let circleObject1;
-let circleObject2;
-let circleObject3;
+let circleArray = [];
+let arraySize = 6;
 
 function setup(){
   var canvas = createCanvas(594, 841); //Use this function to define the size of the output window
   canvas.parent("myContainer");
-
-  circleObject1 = new Circle(random(0, 594), 0, 0.02, random(0.01, 0.08), random(10, 50));
-  circleObject2 = new Circle(random(0, 594), 0, 0.02, random(0.01, 0.08),  random(10, 50));
-  circleObject3 = new Circle(random(0, 594), 0, 0.02, random(0.01, 0.08),  random(10, 50));
+  for (let i=0; i<arraySize; i++){
+    circleArray[i] = new Circle(random(0, 594), 0, 0.02, random(0.01, 0.08), random(10, 70), random(3, 4));
+  }
 }
 
 function draw(){
   fill(0, 10);
   rect(0, 0, width, height);
   //background(0);
-  circleObject1.moveFunction();
-  circleObject1.displayCircle();
-  circleObject2.moveFunction();
-  circleObject2.displayCircle();
-  circleObject3.moveFunction();
-  circleObject3.displayCircle();
+  for (let i=0; i<circleArray.length; i++){
+    circleArray[i].moveFunction();
+    circleArray[i].displayCircle();
+  }
 }
 
 class Circle{
-
-    constructor(x, y, angleX, freq, size){
+    constructor(x, y, angleX, freq, size, speed){
       this.x = x;
       this.y = y;
       this.angleX = angleX;
       this.freq = freq;
       this.size = size;
+      this.speed = speed;
 
       this.rd = random(255);
       this.grn = random(255);
@@ -44,7 +40,7 @@ class Circle{
 
 // The function that controls the movement of the ball
 moveFunction(){
-  this.y = this.y + 1; //Add +1 pixel to the position of x (for every frame)
+  this.y = this.y + this.speed; //Add +1 pixel to the position of x (for every frame)
   //console.log(x);
   //If the position of the ellipse reaches the limit of the canvas,
   //then reset its position to 0 - ellipse appears from the top again
@@ -54,6 +50,9 @@ moveFunction(){
 
   //this.sinValue2 = sin(this.angleX);
   //this.mapSin2 = map(this.sinValue2, -1, 1, 50, -50);
+
+  //let this.currentTime = millis();
+
 
   if (this.y > 866){ //866 comes from 841 (height) + 25 (half size of ellipse)
     this.rd = random (255);
